5d1c3d4244576dc72935196ca65e45e3c98812d6
Tongyinhu <49134227+Tongyinhu@users.noreply.github.com>
2019-04-01 10:04:26 +0800

    提交了一波文件

---
+++ src/main/java/cn/hxzy/controller/T.java
@@ -0,0 +1,33 @@
+package cn.hxzy.controller;
+
+public class T {
+	
+	
+	/**
+	 * 
+	 * 校验器的注解
+	 * 
+	 */
+//	 1 Bean Validation 中内置的 constraint         
+//	 2 @Null   被注释的元素必须为 null    
+//	 3 @NotNull    被注释的元素必须不为 null    
+//	 4 @AssertTrue     被注释的元素必须为 true    
+//	 5 @AssertFalse    被注释的元素必须为 false    
+//	 6 @Min(value)     被注释的元素必须是一个数字，其值必须大于等于指定的最小值    
+//	 7 @Max(value)     被注释的元素必须是一个数字，其值必须小于等于指定的最大值    
+//	 8 @DecimalMin(value)  被注释的元素必须是一个数字，其值必须大于等于指定的最小值    
+//	 9 @DecimalMax(value)  被注释的元素必须是一个数字，其值必须小于等于指定的最大值    
+//	10 @Size(max=, min=)   被注释的元素的大小必须在指定的范围内    
+//	11 @Digits (integer, fraction)     被注释的元素必须是一个数字，其值必须在可接受的范围内    
+//	12 @Past   被注释的元素必须是一个过去的日期    
+//	13 @Future     被注释的元素必须是一个将来的日期    
+//	14 @Pattern(regex=,flag=)  被注释的元素必须符合指定的正则表达式    
+//	15     
+//	16 Hibernate Validator 附加的 constraint    
+//	17 @NotBlank(message =)   验证字符串非null，且长度必须大于0    
+//	18 @Email  被注释的元素必须是电子邮箱地址    
+//	19 @Length(min=,max=)  被注释的字符串的大小必须在指定的范围内    
+//	20 @NotEmpty   被注释的字符串的必须非空    
+//	21 @Range(min=,max=,message=)  被注释的元素必须在合适的范围内 
+
+}
